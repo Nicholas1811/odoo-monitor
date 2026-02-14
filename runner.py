c421acd6a36bd7087bce3ac79f3c268cb40ab8c3
Nicholas <nicholassdl660@gmail.com>
2026-02-14 23:50:31 +0800

    small fix

--- runner.py
+++ runner.py
@@ -3,7 +3,7 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
     page.goto("http://e08g02t03-lab-prod.centralindia.cloudapp.azure.com:8069/")
